275c6aa5690c20b3478d398ba47c86813e71f5c5
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2019-08-15 16:48:18 -0400

    + Added metric and criterion to config

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,2 @@
 python setup.py sdist
-python -m twine upload dist/torch-kerosene-0.0.11.tar.gz
+python -m twine upload dist/torch-kerosene-0.0.12.tar.gz
--- kerosene/config/trainers.py
+++ kerosene/config/trainers.py
@@ -7,7 +7,7 @@ class TrainingConfiguration(object):
 class ModelTrainerConfiguration(object):
 
     def __init__(self, model_name, model_type, model_params, optimizer_type, optimizer_params, scheduler_type,
-                 scheduler_params):
+                 scheduler_params, criterion_type, criterion_params, metric_type, metric_params):
         self._model_name = model_name
         self._model_type = model_type
         self._model_params = model_params
@@ -17,6 +17,12 @@ class ModelTrainerConfiguration(object):
 
         self._scheduler_type = scheduler_type
         self._scheduler_params = scheduler_params
+
+        self._criterion_type = criterion_type
+        self._criterion_params = criterion_params
+
+        self._metric_type = metric_type
+        self._metric_params = metric_params
 
     @property
     def model_name(self):
@@ -46,7 +52,25 @@ class ModelTrainerConfiguration(object):
     def scheduler_params(self):
         return self._scheduler_params
 
+    @property
+    def criterion_type(self):
+        return self._criterion_type
+
+    @property
+    def criterion_params(self):
+        return self._criterion_params
+
+    @property
+    def metric_type(self):
+        return self._metric_type
+
+    @property
+    def metric_params(self):
+        return self._metric_params
+
     @classmethod
     def from_dict(cls, model_name, config_dict):
         cls(model_name, config_dict["type"], config_dict["params"], config_dict["optimizer"]["type"],
-            config_dict["optimizer"]["params"], config_dict["scheduler"]["type"], config_dict["scheduler"]["params"])
+            config_dict["optimizer"]["params"], config_dict["scheduler"]["type"], config_dict["scheduler"]["params"],
+            config_dict["criterion"]["type"], config_dict["criterion"]["params"], config_dict["metric"]["type"],
+            config_dict["metric"]["params"])
