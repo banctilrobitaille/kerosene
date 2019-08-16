bd8182be3b6188028305c30de6f36bca9877b9fe
Pierre-Luc Delisle <pierre-luc.delisle@live.com>
2019-08-16 11:23:26 -0400

    + Added criterions existing in SAMITorch with tests.

--- kerosene/nn/criterions.py
+++ kerosene/nn/criterions.py
@@ -1,8 +1,15 @@
 from enum import Enum
 from typing import Union
 
+import torch
 from torch import nn
 from torch.nn.modules.loss import _Loss
+
+from kerosene.utils import flatten
+from ignite.metrics import MetricsLambda
+
+SUPPORTED_REDUCTIONS = [None, "mean"]
+EPSILON = 1e-15
 
 
 class CriterionType(Enum):
@@ -30,6 +37,9 @@ class CriterionFactory(object):
     def __init__(self):
         super(CriterionFactory, self).__init__()
         self._criterion = {
+            "DiceLoss": DiceLoss,
+            "GeneralizeDiceLoss": GeneralizedDiceLoss,
+            "WeightedCrossEntropyLoss": WeightedCrossEntropyLoss,
             "BCELoss": nn.BCELoss,
             "BCEWithLogtisLoss": nn.BCEWithLogitsLoss,
             "PoissonNLLLoss": nn.PoissonNLLLoss,
@@ -61,3 +71,205 @@ class CriterionFactory(object):
            creator (:obj:`torch.nn.Module`): A torch module object wrapping the new custom criterion function.
         """
         self._criterion[function] = creator
+
+
+class DiceLoss(nn.Module):
+    """
+    The Sørensen-Dice Loss.
+    """
+
+    def __init__(self, reduction: Union[None, str] = "mean"):
+        super(DiceLoss, self).__init__()
+        assert reduction in SUPPORTED_REDUCTIONS, "Reduction type not supported."
+        self._reduction = reduction
+
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor, weights: torch.Tensor = None,
+                ignore_index: int = None):
+        """
+        Computes the Sørensen–Dice loss.
+
+        Note that PyTorch optimizers minimize a loss. In this case, we would like to maximize the dice loss so we
+        return the negated dice loss.
+
+        Args:
+            inputs (:obj:`torch.Tensor`) : A tensor of shape (B, C, ..). The model prediction on which the loss has to
+             be computed.
+            targets (:obj:`torch.Tensor`) : A tensor of shape (B, C, ..). The ground truth.
+            ignore_index (int): An index to ignore for computation.
+            weights (:obj:`torch.Tensor`): A class weight vector of shape (C, ), one per class.
+
+        Returns:
+            :obj:`torch.Tensor`: The Sørensen–Dice loss for each class or reduced according to reduction method.
+        """
+        if ignore_index is not None:
+            if not ignore_index >= 0:
+                raise ValueError("ignore_index must be non-negative, but given {}".format(ignore_index))
+
+        if not inputs.size() == targets.size():
+            raise ValueError("'Inputs' and 'Targets' must have the same shape.")
+
+        inputs = flatten(inputs)
+        targets = flatten(targets)
+
+        targets = targets.float()
+
+        # Compute per channel Dice Coefficient
+        intersect = (inputs * targets).sum(-1)
+
+        if weights is not None:
+            if weights.requires_grad is not False:
+                weights.requires_grad = False
+            intersect = weights * intersect
+
+        denominator = (inputs + targets).sum(-1)
+
+        dice = 1.0 - (2.0 * intersect / denominator.clamp(min=EPSILON))
+
+        if ignore_index is not None:
+
+            def ignore_index_fn(dice_vector):
+                indices = list(range(len(dice_vector)))
+                indices.remove(ignore_index)
+                return dice_vector[indices]
+
+            return MetricsLambda(ignore_index_fn, dice).compute()
+
+        else:
+            if self._reduction == "mean":
+                dice = 1.0 - (2.0 * intersect / denominator.clamp(min=EPSILON)).mean()
+            elif self._reduction is None:
+                pass
+            else:
+                raise NotImplementedError("Reduction method not implemented.")
+            return dice
+
+
+class GeneralizedDiceLoss(torch.nn.Module):
+    """
+      The Generalized Sørensen-Dice Loss.
+      """
+
+    def __init__(self, reduction: Union[None, str] = "mean"):
+        super(GeneralizedDiceLoss, self).__init__()
+        assert reduction in SUPPORTED_REDUCTIONS, "Reduction type not supported."
+        self._reduction = reduction
+
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor, ignore_index: int = None):
+        """
+        Computes the Sørensen–Dice loss.
+
+        Note that PyTorch optimizers minimize a loss. In this case, we would like to maximize the dice loss so we
+        return the negated dice loss.
+
+        Args:
+            inputs (:obj:`torch.Tensor`) : A tensor of shape (B, C, ..). The model prediction on which the loss has to
+             be computed.
+            targets (:obj:`torch.Tensor`) : A tensor of shape (B, C, ..). The ground truth.
+            ignore_index (int): An index to ignore for computation.
+
+        Returns:
+            :obj:`torch.Tensor`: The Sørensen–Dice loss for each class or reduced according to reduction method.
+        """
+        if ignore_index is not None:
+            if not ignore_index >= 0:
+                raise ValueError("ignore_index must be non-negative, but given {}".format(ignore_index))
+
+        if not inputs.size() == targets.size():
+            raise ValueError("'Inputs' and 'Targets' must have the same shape.")
+
+        inputs = flatten(inputs)
+        targets = flatten(targets)
+
+        targets = targets.float()
+
+        class_weights = 1.0 / torch.pow(targets.sum(-1), 2).clamp(min=EPSILON)
+
+        # Compute per channel Dice Coefficient
+        intersect = (inputs * targets).sum(-1) * class_weights
+
+        denominator = (inputs + targets).sum(-1) * class_weights
+
+        dice = 1.0 - (2.0 * intersect / denominator.clamp(min=EPSILON))
+
+        if ignore_index is not None:
+
+            def ignore_index_fn(dice_vector):
+                indices = list(range(len(dice_vector)))
+                indices.remove(ignore_index)
+                return dice_vector[indices]
+
+            return MetricsLambda(ignore_index_fn, dice).compute()
+
+        else:
+            if self._reduction == "mean":
+                dice = 1.0 - (2.0 * intersect / denominator.clamp(min=EPSILON)).mean()
+            elif self._reduction is None:
+                pass
+            else:
+                raise NotImplementedError("Reduction method not implemented.")
+            return dice
+
+
+class WeightedCrossEntropyLoss(torch.nn.Module):
+
+    def __init__(self, reduction="mean"):
+        super(WeightedCrossEntropyLoss, self).__init__()
+        assert reduction in SUPPORTED_REDUCTIONS, "Reduction is not supported."
+        self._reduction = reduction
+
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor, ignore_index: int = None) -> float:
+        """
+        Computes the Weighted Cross Entropy Loss (WCE) as described in https://arxiv.org/pdf/1707.03237.pdf
+
+        Args:
+            inputs (:obj:`torch.Tensor`): A tensor of shape (B, C, ..). The model's prediction on which the loss has to be computed.
+            targets (:obj:`torch.Tensor`): A tensor of shape (B, ..). The ground truth.
+            ignore_index (int): An index to ignore for computation.
+
+       Returns:
+           float: the weighted Cross-Entropy loss value.
+
+        """
+        if ignore_index is not None:
+            self.validate_ignore_index(ignore_index, inputs.shape[1])
+        else:
+            ignore_index = -100
+
+        class_weights = self.compute_class_weights(inputs)
+
+        return torch.nn.functional.cross_entropy(inputs, targets, weight=class_weights,
+                                                 ignore_index=ignore_index)
+
+    @staticmethod
+    def validate_ignore_index(ignore_index: int, input_shape: int):
+        """
+        Validate the `ignore_index` variable.
+
+        Args:
+            ignore_index (int): An index of a class to ignore for computation.
+            input_shape (int): Input tensor.
+        """
+        if ignore_index is not None:
+            if not ignore_index >= 0:
+                raise ValueError("ignore_index must be non-negative, but given {}".format(ignore_index))
+
+        if ignore_index > input_shape:
+            raise ValueError(
+                "ignore index must be lower than the number of classes in confusion matrix, but {} was given.".format(
+                    ignore_index))
+
+    @staticmethod
+    def compute_class_weights(inputs: torch.Tensor):
+        """
+        Compute weights for each class as described in https://arxiv.org/pdf/1707.03237.pdf
+
+        Args:
+            inputs: (:obj:`torch.Tensor`): A tensor of shape (B, C, ..). The model's prediction on which the loss has to be computed.
+
+        Returns:
+            :obj:`torch.Tensor`: A tensor containing class weights.
+        """
+        flattened_inputs = flatten(inputs)
+        class_weights = (flattened_inputs.shape[1] - flattened_inputs.sum(-1)) / flattened_inputs.sum(-1)
+        return class_weights
+
---
+++ tests/nn/test_criterions.py
@@ -0,0 +1,234 @@
+import unittest
+
+import numpy as np
+import torch
+from hamcrest import *
+
+from kerosene.nn.criterions import DiceLoss, GeneralizedDiceLoss, WeightedCrossEntropyLoss
+from kerosene.utils import to_onehot
+
+
+def get_y_true_y_pred():
+    # Generate an image with labels 0 (background), 1, 2
+    # 3 classes:
+    y_true = np.zeros((30, 30), dtype=np.int)
+    y_true[1:11, 1:11] = 1
+    y_true[15:25, 15:25] = 2
+
+    y_pred = np.zeros((30, 30), dtype=np.int)
+    y_pred[5:15, 1:11] = 1
+    y_pred[20:30, 20:30] = 2
+    return y_true, y_pred
+
+
+def compute_tensor_y_true_y_logits(y_true, y_pred):
+    # Create torch.tensor from numpy
+    y_true_tensor = torch.from_numpy(y_true).unsqueeze(0)
+    # Create logits torch.tensor:
+    num_classes = max(np.max(y_true), np.max(y_pred)) + 1
+    y_probas = np.ones((num_classes,) + y_true.shape) * 0.0
+    for i in range(num_classes):
+        y_probas[i, (y_pred == i)] = 1.0
+    y_logits = torch.from_numpy(y_probas).unsqueeze(0).type(torch.float32)
+    return y_true_tensor, y_logits
+
+
+def compute_dice_truth(y_true, y_pred):
+    true_res = [0, 0, 0]
+    for index in range(3):
+        bin_y_true = y_true == index
+        bin_y_pred = y_pred == index
+        intersection = bin_y_true & bin_y_pred
+        true_res[index] = 2.0 * intersection.sum() / (bin_y_pred.sum() + bin_y_true.sum())
+    return true_res
+
+
+def compute_generalized_dice_loss_truth(y_true, y_pred):
+    true_res = [0, 0, 0]
+    for index in range(3):
+        bin_y_true = y_true == index
+        bin_y_pred = y_pred == index
+        weights = (1.0 / (np.sum(bin_y_true) * np.sum(bin_y_true) + 1e-15))
+        intersection = (bin_y_true & bin_y_pred)
+        true_res[index] = 2 * intersection.sum() * weights / (((bin_y_pred.sum() + bin_y_true.sum()) * weights) + 1e-15)
+    return true_res
+
+
+class TestDiceLoss(unittest.TestCase):
+    INVALID_VALUE_1 = -1
+    INVALID_VALUE_2 = "STEVE JOBS"
+    INVALID_VALUE_3 = 10
+    INVALID_VALUE_4 = 11
+    INVALID_REDUCTION = "sum"
+
+    def setUp(self):
+        self.y_true, self.y_pred = get_y_true_y_pred()
+        self.y_true_tensor, self.y_logits = compute_tensor_y_true_y_logits(self.y_true, self.y_pred)
+        self.dice = compute_dice_truth(self.y_true, self.y_pred)
+        self.mean_dice_loss = np.subtract(1.0, np.mean(self.dice))
+
+    def test_should_raise_exception_with_bad_values(self):
+        assert_that(calling(DiceLoss).with_args(reduction=self.INVALID_REDUCTION), raises(AssertionError))
+
+        dice_loss = DiceLoss()
+
+        assert_that(calling(dice_loss.forward).with_args(inputs=None, targets=None),
+                    raises(AttributeError))
+        assert_that(calling(dice_loss.forward).with_args(inputs=self.y_logits, targets=None),
+                    raises(AttributeError))
+        assert_that(calling(dice_loss.forward).with_args(inputs=None, targets=self.y_true_tensor),
+                    raises(AttributeError))
+        assert_that(calling(dice_loss.forward).with_args(inputs=self.y_logits, targets=self.y_true_tensor,
+                                                         ignore_index=self.INVALID_VALUE_1), raises(ValueError))
+        assert_that(calling(dice_loss.forward).with_args(inputs=self.y_logits, targets=self.y_true_tensor,
+                                                         ignore_index=self.INVALID_VALUE_2), raises(TypeError))
+        assert_that(calling(dice_loss.forward).with_args(inputs=self.y_logits, targets=self.y_true_tensor,
+                                                         ignore_index=self.INVALID_VALUE_3), raises(ValueError))
+        assert_that(calling(dice_loss.forward).with_args(inputs=self.y_logits, targets=self.y_true_tensor,
+                                                         ignore_index=self.INVALID_VALUE_4), raises(ValueError))
+
+    def test_should_compute_dice(self):
+        dice_loss = DiceLoss(reduction=None)
+        loss = dice_loss.forward(self.y_logits, to_onehot(self.y_true_tensor, num_classes=3))
+
+        np.testing.assert_almost_equal(loss.numpy(), np.subtract(1.0, self.dice))
+
+    def test_should_compute_dice_for_multiclass_with_ignored_index(self):
+        for ignore_index in range(3):
+            dice_loss = DiceLoss(reduction=None)
+            res = dice_loss.forward(self.y_logits, to_onehot(self.y_true_tensor, num_classes=3),
+                                    ignore_index=ignore_index)
+            true_res = np.subtract(1.0, self.dice[:ignore_index] + self.dice[ignore_index + 1:])
+            np.testing.assert_almost_equal(res.numpy(), true_res), "{}: {} vs {}".format(ignore_index, res, true_res)
+
+    def test_should_compute_mean_dice(self):
+        dice_loss = DiceLoss()
+        loss = dice_loss.forward(self.y_logits, to_onehot(self.y_true_tensor, num_classes=3))
+
+        np.testing.assert_almost_equal(loss.numpy(), self.mean_dice_loss)
+
+    def test_should_compute_mean_dice_for_multiclass_with_ignored_index(self):
+        for ignore_index in range(3):
+            dice_loss = DiceLoss()
+            res = dice_loss.forward(self.y_logits, to_onehot(self.y_true_tensor, num_classes=3),
+                                    ignore_index=ignore_index)
+            true_res = np.subtract(1.0, self.dice[:ignore_index] + self.dice[ignore_index + 1:])
+            np.testing.assert_almost_equal(res.numpy(), true_res), "{}: {} vs {}".format(ignore_index, res, true_res)
+
+
+class TestGeneralizedDiceLoss(unittest.TestCase):
+    INVALID_VALUE_1 = -1
+    INVALID_VALUE_2 = "STEVE JOBS"
+    INVALID_VALUE_3 = 10
+    INVALID_VALUE_4 = 11
+    INVALID_REDUCTION = "sum"
+
+    def setUp(self):
+        self.y_true, self.y_pred = get_y_true_y_pred()
+        self.y_true_tensor, self.y_logits = compute_tensor_y_true_y_logits(self.y_true, self.y_pred)
+        self.generalized_dice_loss = compute_generalized_dice_loss_truth(self.y_true, self.y_pred)
+        self.mean_generalized_dice_loss = np.subtract(1.0, np.mean(self.generalized_dice_loss))
+
+    def test_should_raise_exception_with_bad_values(self):
+        assert_that(calling(GeneralizedDiceLoss).with_args(reduction=self.INVALID_REDUCTION), raises(AssertionError))
+
+        generalized_dice_loss = GeneralizedDiceLoss()
+
+        assert_that(calling(generalized_dice_loss.forward).with_args(inputs=None, targets=None),
+                    raises(AttributeError))
+        assert_that(calling(generalized_dice_loss.forward).with_args(inputs=self.y_logits, targets=None),
+                    raises(AttributeError))
+        assert_that(calling(generalized_dice_loss.forward).with_args(inputs=None, targets=self.y_true_tensor),
+                    raises(AttributeError))
+        assert_that(calling(generalized_dice_loss.forward).with_args(inputs=self.y_logits, targets=self.y_true_tensor,
+                                                                     ignore_index=self.INVALID_VALUE_1),
+                    raises(ValueError))
+        assert_that(calling(generalized_dice_loss.forward).with_args(inputs=self.y_logits, targets=self.y_true_tensor,
+                                                                     ignore_index=self.INVALID_VALUE_2),
+                    raises(TypeError))
+        assert_that(calling(generalized_dice_loss.forward).with_args(inputs=self.y_logits, targets=self.y_true_tensor,
+                                                                     ignore_index=self.INVALID_VALUE_3),
+                    raises(ValueError))
+        assert_that(calling(generalized_dice_loss.forward).with_args(inputs=self.y_logits, targets=self.y_true_tensor,
+                                                                     ignore_index=self.INVALID_VALUE_4),
+                    raises(ValueError))
+
+    def test_should_compute_generalized_dice(self):
+        generalized_dice_loss = GeneralizedDiceLoss()
+        loss = generalized_dice_loss.forward(self.y_logits, to_onehot(self.y_true_tensor, num_classes=3))
+        np.testing.assert_almost_equal(loss.numpy(), self.mean_generalized_dice_loss)
+
+    def test_should_compute_generalized_dice_for_multiclass_with_ignored_index(self):
+        for ignore_index in range(3):
+            generalized_dice_loss = GeneralizedDiceLoss()
+            res = generalized_dice_loss.forward(self.y_logits, to_onehot(self.y_true_tensor, num_classes=3),
+                                                ignore_index=ignore_index)
+            true_res = np.subtract(1.0, self.generalized_dice_loss[:ignore_index] + self.generalized_dice_loss[
+                                                                                    ignore_index + 1:])
+            np.testing.assert_almost_equal(res.numpy(), true_res), "{}: {} vs {}".format(ignore_index, res, true_res)
+
+    def test_should_compute_mean_generalized_dice(self):
+        dice_loss = GeneralizedDiceLoss()
+        loss = dice_loss.forward(self.y_logits, to_onehot(self.y_true_tensor, num_classes=3))
+
+        np.testing.assert_almost_equal(loss.numpy(), self.mean_generalized_dice_loss)
+
+    def test_should_compute_mean_generalized_dice_for_multiclass_with_ignored_index(self):
+        for ignore_index in range(3):
+            dice_loss = GeneralizedDiceLoss()
+            res = dice_loss.forward(self.y_logits, to_onehot(self.y_true_tensor, num_classes=3),
+                                    ignore_index=ignore_index)
+            true_res = np.subtract(1.0, self.generalized_dice_loss[:ignore_index] + self.generalized_dice_loss[
+                                                                                    ignore_index + 1:])
+            np.testing.assert_almost_equal(res.numpy(), true_res), "{}: {} vs {}".format(ignore_index, res, true_res)
+
+
+class TestWeightedCrossEntropy(unittest.TestCase):
+    INVALID_VALUE_1 = -1
+    INVALID_VALUE_2 = "STEVE JOBS"
+    INVALID_VALUE_3 = 10
+    INVALID_VALUE_4 = 11
+    INVALID_REDUCTION = "sum"
+    WEIGHTED_CROSS_ENTROPY_LOSS_TRUTH = 1.0808
+
+    def setUp(self):
+        self.y_true, self.y_pred = get_y_true_y_pred()
+        self.y_true_tensor, self.y_logits = compute_tensor_y_true_y_logits(self.y_true, self.y_pred)
+
+    def test_should_raise_exception_with_bad_values(self):
+        assert_that(calling(WeightedCrossEntropyLoss).with_args(reduction=self.INVALID_REDUCTION),
+                    raises(AssertionError))
+
+        weighted_cross_entropy_loss = WeightedCrossEntropyLoss()
+
+        assert_that(calling(weighted_cross_entropy_loss.forward).with_args(inputs=None, targets=None),
+                    raises(AttributeError))
+        assert_that(calling(weighted_cross_entropy_loss.forward).with_args(inputs=self.y_logits, targets=None),
+                    raises(AttributeError))
+        assert_that(calling(weighted_cross_entropy_loss.forward).with_args(inputs=None, targets=self.y_true_tensor),
+                    raises(AttributeError))
+        assert_that(
+            calling(weighted_cross_entropy_loss.forward).with_args(inputs=self.y_logits, targets=self.y_true_tensor,
+                                                                   ignore_index=self.INVALID_VALUE_1),
+            raises(ValueError))
+        assert_that(
+            calling(weighted_cross_entropy_loss.forward).with_args(inputs=self.y_logits, targets=self.y_true_tensor,
+                                                                   ignore_index=self.INVALID_VALUE_2),
+            raises(TypeError))
+        assert_that(
+            calling(weighted_cross_entropy_loss.forward).with_args(inputs=self.y_logits, targets=self.y_true_tensor,
+                                                                   ignore_index=self.INVALID_VALUE_3),
+            raises(ValueError))
+        assert_that(
+            calling(weighted_cross_entropy_loss.forward).with_args(inputs=self.y_logits, targets=self.y_true_tensor,
+                                                                   ignore_index=self.INVALID_VALUE_4),
+            raises(ValueError))
+
+    def test_should_compute_weights(self):
+        weights = WeightedCrossEntropyLoss.compute_class_weights(self.y_logits)
+        np.testing.assert_almost_equal(weights.numpy(), np.array([0.2857143, 8.0, 8.0]), decimal=7)
+
+    def test_should_return_loss(self):
+        weighted_cross_entropy_loss = WeightedCrossEntropyLoss()
+        loss = weighted_cross_entropy_loss.forward(self.y_logits, self.y_true_tensor)
+        np.testing.assert_almost_equal(loss.numpy(), self.WEIGHTED_CROSS_ENTROPY_LOSS_TRUTH)
