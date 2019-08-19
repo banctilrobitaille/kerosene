d00713e4804696a0e76eb008480aec38ea8fa29d
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2019-08-19 11:02:41 -0400

    - Removed blinker dependency

--- kerosene/events/__init__.py
+++ kerosene/events/__init__.py
@@ -1,18 +1,16 @@
 from enum import Enum
 
-from blinker import signal
-
 
 class Event(Enum):
     ON_TRAINING_BEGIN = "training_begin"
     ON_TRAINING_END = "training_end"
     ON_EPOCH_BEGIN = "epoch_begin"
     ON_TRAIN_EPOCH_BEGIN = "train_epoch_begin"
-    ON_TRAIN_EPOCH_END = signal("train_epoch_end")
-    ON_VALID_EPOCH_BEGIN = signal("valid_epoch_begin")
-    ON_VALID_EPOCH_END = signal("valid_epoch_end")
-    ON_EPOCH_END = signal("epoch_end")
-    ON_TRAIN_BATCH_BEGIN = signal("train_batch_begin")
-    ON_TRAIN_BATCH_END = signal("train_batch_end")
-    ON_VALID_BATCH_BEGIN = signal("valid_batch_begin")
-    ON_VALID_BATCH_END = signal("valid_batch_end")
+    ON_TRAIN_EPOCH_END = "train_epoch_end"
+    ON_VALID_EPOCH_BEGIN = "valid_epoch_begin"
+    ON_VALID_EPOCH_END = "valid_epoch_end"
+    ON_EPOCH_END = "epoch_end"
+    ON_TRAIN_BATCH_BEGIN = "train_batch_begin"
+    ON_TRAIN_BATCH_END = "train_batch_end"
+    ON_VALID_BATCH_BEGIN = "valid_batch_begin"
+    ON_VALID_BATCH_END = "valid_batch_end"
