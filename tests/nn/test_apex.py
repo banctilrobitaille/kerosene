f0df2cac0b9842bb13981a6c51af7f1546968a7f
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2019-08-27 15:40:09 -0400

    * Cleaned tests

--- tests/nn/test_apex.py
+++ tests/nn/test_apex.py
@@ -7,22 +7,26 @@ from kerosene.nn.apex import ApexLoss
 
 
 class TestApexLoss(unittest.TestCase):
+    VALID_LOSS_VALUE = torch.tensor([1.0, 2.0, 3.0])
+    INVALID_LOSS_VALUE = "Ford Fiesta"
+
+    VALID_LOSS_MUL_FACTOR = 2.0
+    INVALID_LOSS_MUL_FACTOR = "Ford Focus"
+
+    VALID_LOSS_ID = 0
+    INVALID_LOSS_ID = "Ford F150"
 
     def test_loss_addition(self):
-        loss_id_1, loss_id_2 = 1, 2
-        loss_value = torch.tensor([1.0, 2.0, 3.0])
-        expected_result = ApexLoss(loss_id_1, torch.tensor([2.0, 4.0, 6.0]), None)
+        expected_result = ApexLoss(self.VALID_LOSS_ID, torch.tensor([2.0, 4.0, 6.0]), None)
 
-        loss1 = ApexLoss(loss_id_1, loss_value, None)
-        loss2 = ApexLoss(loss_id_2, loss_value, None)
+        apex_loss1 = ApexLoss(self.VALID_LOSS_ID, self.VALID_LOSS_VALUE, None)
+        apex_loss2 = ApexLoss(self.VALID_LOSS_ID, self.VALID_LOSS_VALUE, None)
 
-        assert_that(loss1 + loss2, equal_to(expected_result))
+        assert_that(apex_loss1 + apex_loss2, equal_to(expected_result))
 
     def test_factor_multiplication(self):
-        loss_id_1, loss_id_2 = 1, 2
-        loss_value = torch.tensor([1.0, 2.0, 3.0])
-        expected_result = ApexLoss(loss_id_1, torch.tensor([2.0, 4.0, 6.0]), None)
+        expected_result = ApexLoss(self.VALID_LOSS_ID, self.VALID_LOSS_MUL_FACTOR * self.VALID_LOSS_VALUE, None)
 
-        loss = ApexLoss(loss_id_1, loss_value, None)
+        loss = ApexLoss(self.VALID_LOSS_ID, self.VALID_LOSS_VALUE, None)
 
-        assert_that(2 * loss, equal_to(expected_result))
+        assert_that(self.VALID_LOSS_MUL_FACTOR * loss, equal_to(expected_result))
