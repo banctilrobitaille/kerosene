321f0a145ab4226e1935be45eb8c2604d8d092b3
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2019-09-17 09:54:40 -0400

    * Refactored the JS div to avoid shape error

--- kerosene/nn/functional.py
+++ kerosene/nn/functional.py
@@ -3,4 +3,8 @@ import torch.nn.functional as F
 
 
 def js_div(inputs):
-    return torch.sum(F.kl_div(torch.mean(inputs, dim=1, keepdim=True).log(), inputs, reduction='none'), dim=2).mean()
+    js_divs = []
+    for dist in range(inputs.size(1)):
+        js_divs.append(F.kl_div(torch.mean(inputs, dim=1, keepdim=True).log(), inputs[:, dist].unsqueeze(0), reduction='sum'))
+
+    return torch.tensor(js_divs).mean()
