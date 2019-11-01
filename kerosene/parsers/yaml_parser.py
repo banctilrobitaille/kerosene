9c3d2045835ea47551c7d37beddb8e9791d78489
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2019-11-01 15:06:50 -0400

    * Refactored tensor parsing

--- kerosene/parsers/yaml_parser.py
+++ kerosene/parsers/yaml_parser.py
@@ -1,72 +1,27 @@
-import yaml
 import torch
+import yaml
 
 
-class Tensor(yaml.YAMLObject):
-    yaml_tag = u"!pytorch/tensor"
-    yaml_loader = yaml.SafeLoader
+class CustomYamlParser(object):
 
-    def __init__(self, tensor):
-        self._tensor = tensor
-
-    @property
-    def tensor(self):
-        return self._tensor
-
-    @classmethod
-    def from_yaml(cls, loader, node):
+    @staticmethod
+    def parse_tensor(loader, node):
         value = loader.construct_sequence(node, deep=True)
         tensor = torch.Tensor().new_tensor(value)
         return tensor
 
-
-class Tuple(yaml.YAMLObject):
-    yaml_tag = u"!python/tuple"
-    yaml_loader = yaml.SafeLoader
-
-    def __init__(self, tuple_):
-        self._tuple = tuple_
-
-    @property
-    def tuple(self):
-        return self._tuple
-
-    @classmethod
-    def from_yaml(cls, loader, node):
+    @staticmethod
+    def parse_tuple(loader, node):
         value = loader.construct_sequence(node, deep=True)
         tuple_ = tuple(value)
         return tuple_
 
 
-class List(yaml.YAMLObject):
-    yaml_tag = u"!python/list"
-    yaml_loader = yaml.SafeLoader
-
-    def __init__(self, list_):
-        self._list = list_
-
-    @property
-    def list(self):
-        return self._list
-
-    @classmethod
-    def from_yaml(cls, loader, node):
-        value = loader.construct_sequence(node, deep=True)
-        list_ = list(value)
-        return list_
-
-
 class YamlParser(object):
 
     def __init__(self):
-        yaml.SafeLoader.add_constructor(u"!pytorch/tensor", Tensor.from_yaml)
-        yaml.add_constructor(u"!pytorch/tensor", Tensor.from_yaml, Loader=yaml.SafeLoader)
-
-        yaml.SafeLoader.add_constructor(u"!python/tuple", Tuple.from_yaml)
-        yaml.add_constructor(u"!python/tuple", Tuple.from_yaml, Loader=yaml.SafeLoader)
-
-        yaml.SafeLoader.add_constructor(u"!python/list", Tuple.from_yaml)
-        yaml.add_constructor(u"!python/list", Tuple.from_yaml, Loader=yaml.SafeLoader)
+        yaml.SafeLoader.add_constructor(u"!torch/tensor", CustomYamlParser.parse_tensor)
+        yaml.SafeLoader.add_constructor(u"!python/tuple", CustomYamlParser.parse_tuple)
 
     @staticmethod
     def safe_load(file):
--- tests/parsers/yaml_parser_test.py
+++ tests/parsers/yaml_parser_test.py
@@ -1,22 +1,21 @@
-import yaml
 import unittest
+
 import torch
 from hamcrest import *
 
-from kerosene.parsers.yaml_parser import Tensor, YamlParser
+from kerosene.parsers.yaml_parser import YamlParser
 
 
 class YamlParserTest(unittest.TestCase):
 
     def setUp(self) -> None:
-        self._path = "parsers/test_yaml.yml"
+        self._path = "test_yaml.yml"
         self._parser = YamlParser()
 
     def test_should_parse_file_and_create_tensor(self):
-        yaml.SafeLoader.add_constructor('!pytorch/tensor', Tensor.from_yaml)
-        yaml.add_constructor('!pytorch/tensor', Tensor.from_yaml)
-
         with open(self._path) as file:
             config = self._parser.safe_load(file)
 
         assert_that(config["weights"], instance_of(torch.Tensor))
+        assert_that(config["list"], instance_of(list))
+        assert_that(config["tuple"], instance_of(tuple))
