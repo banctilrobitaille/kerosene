6734045946e240774606e677e3f39df80a9778ff
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2019-11-09 12:16:43 -0500

    + Test events and all events

--- kerosene/events/__init__.py
+++ kerosene/events/__init__.py
@@ -25,12 +25,23 @@ class Event(BaseEvent):
     ON_TRAIN_EPOCH_END = "train_epoch_end"
     ON_VALID_EPOCH_BEGIN = "valid_epoch_begin"
     ON_VALID_EPOCH_END = "valid_epoch_end"
+    ON_TEST_EPOCH_BEGIN = "test_epoch_begin"
+    ON_TEST_EPOCH_END = "test_epoch_end"
     ON_EPOCH_END = "epoch_end"
+    ON_BATCH_BEGIN = "batch_begin"
     ON_TRAIN_BATCH_BEGIN = "train_batch_begin"
     ON_TRAIN_BATCH_END = "train_batch_end"
     ON_VALID_BATCH_BEGIN = "valid_batch_begin"
     ON_VALID_BATCH_END = "valid_batch_end"
+    ON_TEST_BATCH_BEGIN = "valid_batch_begin"
+    ON_TEST_BATCH_END = "valid_batch_end"
     ON_BATCH_END = "batch_end"
+
+    EPOCH_EVENTS = [ON_EPOCH_BEGIN, ON_TRAIN_EPOCH_BEGIN, ON_TRAIN_EPOCH_END, ON_VALID_EPOCH_BEGIN, ON_VALID_EPOCH_END,
+                    ON_EPOCH_END, ON_TEST_EPOCH_BEGIN, ON_TEST_BATCH_BEGIN, ON_TEST_BATCH_END]
+    TRAIN_BATCH_EVENTS = [ON_BATCH_BEGIN, ON_TRAIN_BATCH_BEGIN, ON_TRAIN_BATCH_END, ON_BATCH_END]
+    VALID_BATCH_EVENTS = [ON_BATCH_BEGIN, ON_VALID_BATCH_BEGIN, ON_VALID_BATCH_END, ON_BATCH_END]
+    TEST_BATCH_EVENTS = [ON_BATCH_BEGIN, ON_TEST_BATCH_BEGIN, ON_TEST_BATCH_END, ON_BATCH_END]
 
 
 class Monitor(BaseVariable):
