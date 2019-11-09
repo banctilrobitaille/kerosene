adea4406962d52c49259ec9a9922aa826dd63e45
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2019-11-09 17:02:15 -0500

    * Fixed tests

--- tests/events/handlers/console.py
+++ tests/events/handlers/console.py
@@ -3,7 +3,7 @@ import unittest
 
 from mockito import verify, spy
 
-from kerosene.events.handlers.console import PrintTrainingStatus
+from kerosene.events.handlers.console import PrintTrainingStatus, StatusConsoleColorPalette
 from kerosene.training import Status
 
 
@@ -13,17 +13,16 @@ class ConsoleHandlerTest(unittest.TestCase):
         logging.basicConfig(level=logging.INFO)
         self._logger_mock = spy(logging.getLogger())
 
-
     def test_print_status_with_colors(self):
         expected_string = "\nCurrent state: [92mTraining[0m |  Epoch: 0 | Training step: 0 | Validation step: 0 \n"
-        handler = PrintTrainingStatus(1)
+        handler = PrintTrainingStatus(colors=StatusConsoleColorPalette.DEFAULT)
         handler.LOGGER = self._logger_mock
         handler.print_status(Status.TRAIN, 0, 0, 0)
         verify(self._logger_mock).info(expected_string)
 
     def test_print_status_without_colors(self):
         expected_string = "\nCurrent state: Training\x1b[0m |  Epoch: 0 | Training step: 0 | Validation step: 0 \n"
-        handler = PrintTrainingStatus(1, is_colored=False)
+        handler = PrintTrainingStatus()
         handler.LOGGER = self._logger_mock
         handler.print_status(Status.TRAIN, 0, 0, 0)
         verify(self._logger_mock).info(expected_string)
