a6ef4382333565732c9ffe950728401bcd086f77
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2020-01-15 11:50:02 -0500

    * Changed ModelState to Monitors

--- kerosene/events/handlers/visdom.py
+++ kerosene/events/handlers/visdom.py
@@ -39,53 +39,26 @@ class BaseVisdomHandler(EventHandler, ABC):
         return [item for sublist in list_of_visdom_data for item in sublist]
 
 
-class PlotAllModelStateVariables(BaseVisdomHandler):
+class PlotMonitors(BaseVisdomHandler):
     SUPPORTED_EVENTS = [Event.ON_EPOCH_END, Event.ON_TRAIN_BATCH_END, Event.ON_VALID_BATCH_END, Event.ON_TEST_BATCH_END,
                         Event.ON_BATCH_END]
 
     def __init__(self, visdom_logger: VisdomLogger, every=1):
         super().__init__(visdom_logger, every)
+        self._plot_losses = PlotLosses(visdom_logger, every)
+        self._plot_metrics = PlotMetrics(visdom_logger, every)
 
     def __call__(self, event: TemporalEvent, monitors: dict, trainer: Trainer):
         assert event in self.SUPPORTED_EVENTS, "Unsupported event provided. Only {} are permitted.".format(
             self.SUPPORTED_EVENTS)
-        data = None
 
-        if self.should_handle_epoch_data(event, trainer.epoch):
-            data = list(map(lambda model_state: self.create_epoch_visdom_data(trainer.epoch, model_state),
-                            trainer.model_trainers))
-        elif self.should_handle_step_data(event, trainer.current_train_step):
-            data = list(map(
-                lambda model_state: self.create_train_batch_visdom_data(trainer.current_train_step, model_state),
-                trainer.model_trainers))
-        elif self.should_handle_step_data(event, trainer.current_valid_step):
-            data = list(map(
-                lambda model_state: self.create_valid_batch_visdom_data(trainer.current_valid_step, model_state),
-                trainer.model_trainers))
+        if self.should_handle(event):
+            self._plot_losses(event, monitors, trainer)
+            self._plot_metrics(event, monitors, trainer)
 
-        if data is not None:
-            self.visdom_logger(self.flatten(data))
-
-    @staticmethod
-    def create_epoch_visdom_data(epoch, model_trainer: ModelTrainer):
-        data = PlotLosses.create_epoch_visdom_data(epoch, model_trainer)
-        data.extend(PlotMetrics.create_epoch_visdom_data(epoch, model_trainer))
-
-        return data
-
-    @staticmethod
-    def create_train_batch_visdom_data(step, model_trainer: ModelTrainer):
-        data = PlotLosses.create_train_batch_visdom_data(step, model_trainer)
-        data.extend(PlotMetrics.create_train_batch_visdom_data(step, model_trainer))
-
-        return data
-
-    @staticmethod
-    def create_valid_batch_visdom_data(step, model_trainer: ModelTrainer):
-        data = PlotLosses.create_valid_batch_visdom_data(step, model_trainer)
-        data.extend(PlotMetrics.create_valid_batch_visdom_data(step, model_trainer))
-
-        return data
+    def create_visdom_data(self, event, model_name, monitors):
+        data = self._plot_losses.create_visdom_data(event, model_name, monitors)
+        data.extend(self._plot_metrics.create_visdom_data(event, model_name, monitors))
 
 
 class PlotLosses(BaseVisdomHandler):
@@ -118,7 +91,7 @@ class PlotLosses(BaseVisdomHandler):
         if data is not None:
             self.visdom_logger(self.flatten(data))
 
-    def create_visdom_data(self):
+    def create_visdom_data(self, event, model_name, monitors):
         pass
 
     @staticmethod
@@ -208,6 +181,9 @@ class PlotCustomVariables(BaseVisdomHandler):
         if data is not None:
             self.visdom_logger(data)
 
+    def create_visdom_data(self):
+        pass
+
     def create_epoch_visdom_data(self, trainer: Trainer):
         return [VisdomData(trainer.name, self._variable_name, self._plot_type, PlotFrequency.EVERY_EPOCH,
                            [trainer.epoch], trainer.custom_variables[self._variable_name], self._params)]
@@ -231,6 +207,9 @@ class PlotLR(BaseVisdomHandler):
         assert event in self.SUPPORTED_EVENTS, "Unsupported event provided. Only {} are permitted.".format(
             self.SUPPORTED_EVENTS)
         data = None
+
+        if self.should_handle(event):
+            pass
 
         if self.should_handle_epoch_data(event, trainer.epoch):
             data = list(map(lambda model_state: self.create_epoch_visdom_data(trainer.epoch, model_state),
