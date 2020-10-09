7e606fc521edd887e36710718109db8f91ec1090
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2020-10-09 13:53:33 -0400

    + Version up

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
 python setup.py sdist
-python -m twine upload dist/torch-kerosene-0.2.3.tar.gz
+python -m twine upload dist/torch-kerosene-0.2.4.tar.gz
 
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 python3 setup.py sdist
-python3 -m twine upload dist/torch-kerosene-0.2.2.tar.gz
+python3 -m twine upload dist/torch-kerosene-0.2.4.tar.gz
